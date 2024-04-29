c0bd230c862e30e473040c6a96f353ccf5931375
Jeffrey <69681552+Jeffrey9427@users.noreply.github.com>
2024-04-30 00:28:26 +0700

    Update FindRescalePackage.jsx

--- frontend/src/pages/xyz/xyz_mobile/FindRescalePackage.jsx
+++ frontend/src/pages/xyz/xyz_mobile/FindRescalePackage.jsx
@@ -1,11 +1,35 @@
 import NavbarXYZ from '../../../components/xyz/xyz_mobile/NavbarXYZ.jsx'
 import SearchRescale from '../../../components/xyz/xyz_mobile/SearchRescale.jsx'
+import { useState, useEffect } from 'react'
 
 function FindRescalePackage() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+        setIsMobile(window.innerWidth < 600);
+        };
+
+        // Initial check on component mount
+        handleResize();
+
+        // Add event listener to update isMobile when window is resized
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener on component unmount
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className='bg-quaternary w-screen h-screen overflow-y-scroll'>
-            <SearchRescale />
-            <NavbarXYZ />
+            {isMobile && (
+                <>
+                    <SearchRescale />
+                    <NavbarXYZ />
+                </>
+            )}
         </div>
     )
 }
