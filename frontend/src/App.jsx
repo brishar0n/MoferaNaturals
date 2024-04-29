a1b8b06103113bf57505c626f6fb1cff0166a84b
Felix <ferdinandfelix23@gmail.com>
2024-04-29 14:38:34 +0700

    Add authentication components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
 import './style/App.css'
 import Homepage from './pages/Homepage';
 import Login from './components/auth/Login';
+import Verification from './components/auth/Verification';
+import ResetPassword from './components/auth/ResetPassword';
 import WelcomeBack from './pages/WelcomeBack';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from './components/auth/Register';
+import Notify from './components/notif/notify';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} exact />
-        <Route path="/login" element={<Login />} exact />
         <Route path="/welcomeback" element={<WelcomeBack />} exact />
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/register" element={<Register />} exact />
+        <Route path="/verification" element={<Verification />} exact/>
+        <Route path="/resetpass" element={<ResetPassword />} exact/>
+        <Route path="/notify" element={<Notify/>} exact/>
       </Routes>
     </Router>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,9 +3,21 @@ export default {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+      ],
+      theme: {
+        extend: {
+          borderRadius: {
+            "4xl":"4.8rem"
+          },
+          colors: {
+            primary: 'var(--color-primary)',
+            secondary: 'var(--color-secondary)',
+            tertiary: 'var(--color-tertiary)',
+            quaternary: 'var(--color-quaternary)',
+            quinary: 'var(--color-quinary)',
+            senary: 'var(--color-senary)',
+          },
+        },
+      },
+      plugins: [],
+    };
