373c5cbfd5507c80fb9f585629c02d3690c79806
Jeffrey <69681552+Jeffrey9427@users.noreply.github.com>
2024-04-30 21:18:11 +0700

    Update RescaleHeader.jsx

--- frontend/src/components/xyz/xyz_mobile/RescaleHeader.jsx
+++ frontend/src/components/xyz/xyz_mobile/RescaleHeader.jsx
@@ -6,16 +6,16 @@ import '../../../style/xyz/xyz_mobile/RescalePackage.css';
 function RescaleHeader({ handleBack }) {
     return (
         <div>
-            <div className="flex place-items-end flex-col p-3 text-right relative h-60 h-96">
-                <button className='absolute bg-tertiary p-2 rounded-full left-5 text-primary top-5' onClick={handleBack}>
+            <div className="flex place-items-end flex-col pt-3 px-3 text-right relative h-60 h-96">
+                <button className='absolute bg-tertiary p-2 rounded-full left-5 text-white top-5' onClick={handleBack}>
                     <GoChevronLeft className='w-6 h-6'/>
                 </button>
                 <div className="absolute left-0 mt-5">
                     <img src={rescaleElement} alt="rescaleElement"/>
                 </div>
                 <div className="w-1/2 mr-3 text-position">
-                    <p className='text-3xl secondary font-bold mb-1 mt-5'>Rescaling</p>
-                    <p className='secondary mb-1 leading-none leading-normal'>Scale the package information for safety precautions </p>
+                    <p className='text-3xl primary font-bold mb-1 mt-5'>Rescaling</p>
+                    <p className='primary leading-norm leading-none'>Scale the package information for safety precautions </p>
                 </div>
             </div>
         </div>
