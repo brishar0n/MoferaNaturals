bf520eb06a6c96824bd47f90b4faf57b8360179e
Jeffrey <69681552+Jeffrey9427@users.noreply.github.com>
2024-04-30 21:17:38 +0700

    Update PackageDetails.jsx

--- frontend/src/components/xyz/xyz_mobile/PackageDetails.jsx
+++ frontend/src/components/xyz/xyz_mobile/PackageDetails.jsx
@@ -4,7 +4,7 @@ import '../../../style/xyz/xyz_mobile/RescalePackage.css';
 
 function PackageDetails({ packageData }) {
     return (
-        <div className='bg-white mb-3 w-3/4 mx-auto p-4 rounded-2xl text-left relative mt-5'>
+        <div className='bg-white mb-5 w-3/4 mx-auto p-4 rounded-2xl text-left relative mt-5'>
             <img src={vectorpackage} alt="package" className='absolute ml-5 vector'/>
             <p className='primary font-bold text-center mb-2 title'>Package ID #{packageData.packageID}</p>
             <p className='items-start primary text-xs mb-1 font-medium'>Package Information</p>
