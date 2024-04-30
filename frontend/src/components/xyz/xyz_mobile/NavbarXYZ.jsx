7b3a444417c269c0d3b0dc3205bcfeb0edbba7f7
Jeffrey <69681552+Jeffrey9427@users.noreply.github.com>
2024-04-30 16:54:57 +0700

    Add Link to findRescale page

--- frontend/src/components/xyz/xyz_mobile/NavbarXYZ.jsx
+++ frontend/src/components/xyz/xyz_mobile/NavbarXYZ.jsx
@@ -1,10 +1,14 @@
 import rescale from '../../../assets/xyz/rescale.svg';
 import reception from '../../../assets/xyz/reception.svg';
+import { Link } from "react-router-dom";
 
 function NavbarXYZ() {
     return (
         <div className="bg-secondary flex flex-row justify-evenly py-6 absolute w-full bottom-0 rounded-t-3xl fixed">
-            <button><img src={rescale} alt="rescale" className='w-12'/></button>
+            <Link to="/findrescale">
+                <button><img src={rescale} alt="rescale" className='w-12'/></button>
+            </Link>
+        
             <button><img src={reception} alt="reception" className='w-12'/></button>
         </div>
     )
