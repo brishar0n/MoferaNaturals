8523705cbbb392ad39b651b6638df98b01876cf2
Jeffrey <69681552+Jeffrey9427@users.noreply.github.com>
2024-04-30 21:19:23 +0700

    Update SearchRescale.jsx

--- frontend/src/components/xyz/xyz_mobile/SearchRescale.jsx
+++ frontend/src/components/xyz/xyz_mobile/SearchRescale.jsx
@@ -33,14 +33,14 @@ function SearchRescale() {
         <div className='pb-36'>
             <div className="flex items-center justify-center flex-col relative">
                 <div>
-                    <p className='text-3xl secondary font-bold mb-1 mt-5'>Rescaling</p>
-                    <p className='secondary mb-1'>Find Package ID to rescale</p>
+                    <p className='text-3xl primary font-bold mb-1 mt-5'>Rescaling</p>
+                    <p className='primary mb-1'>Find Package ID to rescale</p>
                 </div>
                 <div className='w-10 h-10 rounded-full bg-gray-400 absolute right-5'>
                     <img src="" alt="" />
                 </div>
             </div>
-            <div className='search relative py-4'>
+            <div className='search relative py-4 mb-3'>
                 <input type="search" placeholder='Search Package ID...' value={input} onChange={(e) => handleSearch(e.target.value)}/>
                 <button className='absolute right-8 top-1/2 p-3 rounded-full -translate-y-1/2 text-white'>
                     <AiOutlineSearch/>
