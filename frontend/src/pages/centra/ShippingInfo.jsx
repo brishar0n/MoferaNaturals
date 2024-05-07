b03f871136b86fbfd2b42949bc2ec9ab4f899bdd
Tyrone Vargas <77775585+tyroneley@users.noreply.github.com>
2024-05-07 22:55:46 +0700

    shipping info FIX 1

--- frontend/src/pages/centra/ShippingInfo.jsx
+++ frontend/src/pages/centra/ShippingInfo.jsx
@@ -1,5 +1,6 @@
 // Tyrone
-import { useState, useEffect } from 'react' 
+import React, { useState, useEffect } from 'react' 
+import { useNavigate } from "react-router-dom";
 import box from '../../assets/shipping/box-with-tape.svg'
 import backarrow from '../../assets/shipping/back_arrow.svg'
 import bgupper from '../../assets/shipping/bg_upper.svg'
@@ -8,98 +9,114 @@ import bglowerright from '../../assets/shipping/bg_lower_right.svg'
 import menubar from '../../assets/shipping/menu_bar.svg'
 import mascot from '../../assets/shipping/mascot.svg'
 import ShippingCard from '../../components/centra/ShippingCard'
+import NavigationBar from "../../components/Navbar.jsx";
 import '../../style/Shipping.css'
 
 export const ShippingInfo = () => {
-    const [isMobile, setIsMobile] = useState(false);
-
+    const [weight, setWeight] = useState(0);
+    const [date, setDate] = useState(new Date());
+    const [isMobile, setIsMobile] = React.useState(false);
+    const navigate = useNavigate();
+  
+    const handleWeightChange = (event) => {
+      setWeight(event.target.value);
+    };
+  
+    const handleDateChange = (event) => {
+      setDate(event.target.value);
+    };
+  
     useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth < 600);
-        };
-
-        // Initial check on component mount
-        handleResize();
-
-        // Add event listener to update isMobile when window is resized
-        window.addEventListener('resize', handleResize);
-
-        // Clean up event listener on component unmount
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
+      function handleResize() {
+        setIsMobile(window.innerWidth < 600);
+      }
+  
+      handleResize();
+      window.addEventListener("resize", handleResize);
+  
+      return () => window.removeEventListener("resize", handleResize);
     }, []);
-
+  
+    const handleBack = () => navigate("/dashboard");
+  
+    const handleWash = () => navigate("/washwetleaves");
+  
+    const handleDry = () => navigate("/drywetleaves");
+  
     return (
-        <div className='bg-quaternary h-screen'>
-            {isMobile && (
-                <>
-                    <div className='h-screen'>
-                        <img src={ bgupper } className='w-screen absolute'/>
+      <div>
+        {isMobile && (
+          <>
+            <div className="overflow-auto h-[calc(100vh-6rem)] md:h-auto bg-primary min-h-screen flex flex-col items-center overflow-auto resize-none pb-24">
 
-                        <img src={ backarrow } className='absolute top-11 left-7'/>
+            <img src={bgupper} className='w-screen absolute '/>
 
-                        <p className='text-white text-4xl font-bold absolute text-left top-32 right-48'> Shipping <br></br> Information</p>
+            <img src={box} className='absolute top-12 right-7 z-51'/>
 
-                        <ShippingCard>
-                            <div className='text-left ml-16 mt-5'>
-                                <label htmlFor='package-weight' className='text-primary font-medium'> Package Weight: <br></br></label>
-                                <input
-                                    name='package-weight'
-                                    id='package-weight'
-                                    className='border border-gray-300 rounded-md w-5/6 px-3 py-1.5 bg-quinary'
-                                />
-                            </div>
+            <img src={backarrow} className="absolute top-11 left-10" onClick={() => {navigate("/login");}}/>
+              
+            <p className='text-white text-4xl font-bold absolute text-left top-32 left-14 '> Shipping <br></br> Information </p>
 
-                            <div className='text-left ml-16 mt-5'>
-                                <label htmlFor='total-packages' className='text-primary font-medium'> Total Packages: <br></br></label>
-                                <input
-                                    name='total-packages'
-                                    id='total-packages'
-                                    className='border border-gray-300 rounded-md w-5/6 px-3 py-1.5 bg-quinary'
-                                />
-                            </div>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+              <div className="content flex flex-col items-center">
+                <div className="bg-white p-4 rounded-xl shadow-md z-50">
+                  <div className="flex item s-center mb-4">
+                  <span className="text-gray-500 p-2">Package Weight:</span>
+                    <input
+                      type="number"
+                      value={weight}
+                      onChange={handleWeightChange}
+                      className="bg-green-700 bg-opacity-20 rounded-lg px-2 py-1 mr-2"
+                    />
+                  </div>
 
-                            <div className='text-left ml-16 mt-5'>
-                                <label htmlFor='expedition' className='text-primary font-medium'> Expedition: <br></br></label>
-                                <input
-                                    name='expedition'
-                                    id='expedition'
-                                    className='border border-gray-300 rounded-md w-5/6 px-3 py-1.5 bg-quinary'
-                                />
-                            </div>
+                  <div className="flex items-center mb-4">
+                  <span className="text-gray-500 p-2">Total Packages:</span>
+                    <input
+                      type="number"
+                      value={weight}
+                      onChange={handleWeightChange}
+                      className="bg-green-700 bg-opacity-20 rounded-lg px-2 py-1 mr-2"
+                    />
+                  </div>
 
-                            <div className='text-left ml-16 mt-5'>
-                                <label htmlFor='date' className='text-primary font-medium'> Date: <br></br></label>
-                                <input
-                                    name='date'
-                                    id='date'
-                                    className='border border-gray-300 rounded-md w-5/6 px-3 py-1.5 bg-quinary'
-                                />
-                            </div>
+                  <div className="flex items-center mb-4">
+                  <span className="text-gray-500 p-2">Expedition:</span>
+                    <input
+                      type="text"
+                      value={weight}
+                      onChange={handleWeightChange}
+                      className="bg-green-700 bg-opacity-20 rounded-lg px-2 py-1 mr-2"
+                    />
+                  </div>
 
-                            <div className='mt-8'>
-                                <button className='rounded-full bg-secondary text-white font-bold px-4 py-2 w-36 btn-login'> NOTIFY MOFERA </button>
-                            </div>
+                  <div className="flex items-center">
+                  <span className="text-gray-500 p-2">Date:</span>
+                    <input
+                      type="date"
+                      value={date.toISOString().substring(0, 10)}
+                      onChange={handleDateChange}
+                      className="bg-green-700 bg-opacity-20 rounded-lg px-2 py-1 mr-2"
+                    />
+                  </div>
+                  <button className="bg-green-700 hover:bg-green-700 text-white font-bold py-2 px-12 rounded-2xl mt-3">
+                    SHIP
+                  </button>
+                  <br></br>
+                  <button className="bg-green-700 hover:bg-green-700 text-white font-bold py-2 px-12 rounded-2xl mt-3">
+                    NOTIFY MOFERA
+                  </button>
 
-                            <br></br><br></br>
-
-                            <div className='mt-8'>
-                                <button className='rounded-full bg-secondary text-white font-bold px-4 py-2 w-36 btn-login'> Login </button>
-                            </div>
-
-                            <img src={mascot} className='absolute bottom-12 right-10' />
-                        </ShippingCard>
-
-                        <div className="absolute bottom-0">
-                            <img src={bglowerleft} className='w-screen absolute' />
-                            <img src={bglowerright} className='w-screen absolute' />
-                            <div className="menu_bar">
-                                <img src={menubar} className='w-screen absolute' />
-                            </div>
-                        </div>
-                    </div>
-                </>
+                </div>
+                  <img src={mascot} className="absolute ml-80 mt-52 z-50"/>
+                
+              </div>
+                <img src={bglowerleft} className="w-screen absolute bottom-0"/>
+                <img src={bglowerright} className="w-screen absolute bottom-0"/>
+            </div>
+            
+            <NavigationBar/>
+          </>
             )}
         </div>
     )
