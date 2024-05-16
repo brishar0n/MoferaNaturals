c0747304d284a1dd23638707f15039cf347fe1c1
Brigitte <114067290+brishar0n@users.noreply.github.com>
2024-05-17 03:39:50 +0700

    commit

---
+++ frontend/src/pages/xyz/xyz_desktop/CentraActivityMonitor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from '../../../components/xyz/Sidebar'
+import "../../../style/Desktop.css"
+
+const CentraActivityMonitor = () => {
+  return (
+    <Sidebar/>
+  )
+}
+
+export default CentraActivityMonitor
