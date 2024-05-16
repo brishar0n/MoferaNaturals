4fe550990f448a28cb01198b84fda9b2e6c8f8e7
Brigitte <114067290+brishar0n@users.noreply.github.com>
2024-05-17 03:37:43 +0700

    adding details + dropdown menu

--- frontend/src/components/xyz/Sidebar.jsx
+++ frontend/src/components/xyz/Sidebar.jsx
@@ -1,28 +1,55 @@
 import { useState, useEffect } from 'react'
-import moferalogowhite from '../../assets/xyz/mofera-logo-white.svg'
-import menu from '../../assets/xyz/Menu.svg'
+import desktoplogo from '../../assets/desktop/desktoplogo.svg'
+import hamburger from '../../assets/desktop/hamburger.svg'
+import desktoplogout from '../../assets/desktop/desktoplogout.svg'
 
 function Sidebar() {
+
+    const [isActive, setIsActive] = useState(false);
+    const [dashboardOpen, setDashboardOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setDashboardOpen(!dashboardOpen);
+    };
+
     return (
-        <div className="bg-octonary hidden lg:block h-screen w-1/5 flex flex-col justify-between items-center">
-            <div className='flex flex-col justify-center items-center gap-8'>
-                <button>
-                    <img className='h-14 absolute top-4 left-10' src={menu}></img>
-                </button>
+        <div className="bg-primary hidden lg:block h-screen w-1/5 flex flex-col justify-between items-center">
+            <div className='bg-primary flex flex-col justify-center items-center gap-8'>
+                <div className="bg-primary flex items-center w-full p-5">
+                    <button>
+                        <img className='bg-primary h-6 w-6 ml-3' src={hamburger}></img>
+                    </button>
+                    <span className="bg-primary ml-4">
+                        <img className='bg-primary h-auto w-auto ml-3' src={desktoplogo}></img>
+                    </span>
+                </div>
 
-                <br></br>
-        
-                <img className='h-auto w-1/2 max-w-full mt-16' src={moferalogowhite}></img>
-
-                <div className='flex flex-col justify-center items-center gap-5 w-full mt-5'>
+                <div className='bg-primary flex flex-col justify-center items-center gap-5 w-full mt-5'>
                     <nav>
                         <ul>
-                            <li className="py-4 flex justify-start items-center w-full hover:bg-green-700 hover:text-white cursor-pointer text-black">Dashboard</li>
-                            <li className="py-4 flex justify-start items-center w-full hover:bg-green-700 hover:text-white cursor-pointer text-black">Shipment Tracker</li>
-                            <li className="py-4 flex justify-start items-center w-full hover:bg-green-700 hover:text-white cursor-pointer text-black">Checkpoint</li>
-                            <li className="py-4 flex justify-start items-center w-full hover:bg-green-700 hover:text-white cursor-pointer text-black">Arrived Package</li>
-                            <li className="py-4 flex justify-start items-center w-full hover:bg-green-700 hover:text-white cursor-pointer text-black">Notifications</li>
-                            <li className="py-4 flex justify-start items-center w-full hover:bg-green-700 hover:text-white cursor-pointer text-black">Centra Activity Monitor</li>
+                            <li 
+                                onClick={toggleDropdown}
+                                className="bg-primary py-4 px-8 flex justify-start items-center w-full hover:bg-white hover:text-green-800 font-medium cursor-pointer text-white">Dashboard</li>
+
+                            {dashboardOpen && (
+                                <>
+                                    <li className="bg-primary py-2 px-4 text-white hover:bg-white hover:text-green-800 cursor-pointer">
+                                        Wet Leaves
+                                    </li>
+                                    <li className="bg-primary py-2 px-4 text-white hover:bg-white hover:text-green-800 cursor-pointer">
+                                        Dry Leaves
+                                    </li>
+                                    <li className="bg-primary py-2 px-4 text-white hover:bg-white hover:text-green-800 cursor-pointer">
+                                        Flour
+                                    </li>
+                                </>
+                            )}
+
+                            <li className="bg-primary py-4 px-8 flex justify-start items-center w-full hover:bg-white hover:text-green-800 font-medium cursor-pointer text-white">Shipment Tracker</li>
+                            <li className="bg-primary py-4 px-8 flex justify-start items-center w-full hover:bg-white hover:text-green-800 font-medium cursor-pointer text-white">Checkpoint</li>
+                            <li className="bg-primary py-4 px-8 flex justify-start items-center w-full hover:bg-white hover:text-green-800 font-medium cursor-pointer text-white">Arrived Package</li>
+                            <li className="bg-primary py-4 px-8 flex justify-start items-center w-full hover:bg-white hover:text-green-800 font-medium cursor-pointer text-white">Notifications</li>
+                            <li className="bg-primary py-4 px-8 flex justify-start items-center w-full hover:bg-white hover:text-green-800 font-medium cursor-pointer text-white">Centra Activity Monitor</li>
                         </ul>
                     </nav>
                 </div>
@@ -30,7 +57,9 @@ function Sidebar() {
 
             <div className='flex-grow'></div>
 
-            <button className="py-4 w-full hover:bg-red-600 hover:text-white">Sign Out</button>
+            <button>
+                <img src={desktoplogout} className='bg-primary mt-36 mr-44'></img>
+            </button>
         </div>
     )
 }
