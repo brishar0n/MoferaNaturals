ccd8f2975d4a9565375292e70548a3e18d5e0e8f
Jeff <jeffguo0927@gmail.com>
2024-05-26 16:18:24 +0700

    edit add powder page

--- frontend/src/pages/centra/AddPowder.jsx
+++ frontend/src/pages/centra/AddPowder.jsx
@@ -3,19 +3,26 @@ import "../../style/App.css"
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavigationBar from "../../components/centra/CentraNavbar";
+import PowderForm from "../../components/centra/PowderForm";
 
 function AddPowder(){
     const [weight, setWeight] = useState(0);
-    const [date, setDate] = useState(new Date());
+    const [driedDate, setDriedDate] = useState(new Date());
+    const [flouredDate, setFlouredDate] = useState(new Date());
     const [isMobile, setIsMobile] = React.useState(false);
+    const [formSubmitted, setFormSubmitted] = useState(false);
     const navigate = useNavigate();
 
     const handleWeightChange = (event) => {
         setWeight(event.target.value);
     };
 
-    const handleDateChange = (event) => {
-        setDate(event.target.value);
+    const handleDriedDateChange = (event) => {
+        setDriedDate(event.target.value);
+    };
+
+    const handleFlouredDateChange = (event) => {
+        setFlouredDate(event.target.value);
     };
 
     useEffect(() => {
@@ -29,14 +36,22 @@ function AddPowder(){
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
-    const handleBack = () => navigate("/homepage");
+    const handleBack = () => navigate("/dashboard");
+
+    const handleHistory = () => navigate("/viewpowder");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setFormSubmitted(true);
+    }
     
     return (
       <div>
         {isMobile && (
           <>
-            <div className="overflow-auto h-[calc(100vh-6rem)] md:h-auto bg-quaternary min-h-screen flex flex-col items-center overflow-auto resize-none pb-24">
+            <div className="overflow-auto h-[calc(100vh-6rem)] md:h-auto bg-quaternary min-h-screen flex flex-col items-center overflow-auto resize-none pb-36">
                 <img src="src/assets/AddPage/frameAdd.svg" className="absolute top-100vh w-screen z-0"></img>
+                <img src="src/assets/AddPage/mascotAdd.svg" className="absolute left-6 top-96 pt-28 z-40"></img>
 
                 <div className="flex pt-16 gap-11 pr-20 z-10">
                     <img src="src/assets/common/backarrow.svg" onClick={handleBack}></img>
@@ -45,66 +60,29 @@ function AddPowder(){
 
                 <br></br>
 
-                <div className="bg-white w-2/3 h-1/4 rounded-2xl z-30">
-                    <div className="flex flex-col justify-center items-center pt-5">
-                        <label htmlFor="weight" className="font-medium text-sm pr-44 mb-1">Weight:</label>
-                        <input 
-                        type="number" 
-                        value={weight} 
-                        onChange={handleWeightChange}
-                        className="w-4/5 bg-quinary rounded-md pl-2"></input>
-                    </div>
-
-                    <div className="flex flex-col justify-center items-center pt-4">
-                        <label htmlFor="date" className="font-medium text-sm pr-48 mb-1">Date:</label>
-                        <input 
-                        type="date" 
-                        value={date.toISOString().substring(0, 10)} 
-                        onChange={handleDateChange}
-                        className="w-4/5 bg-quinary rounded-md pl-2"></input>
-                    </div>
-
-                    <br></br>
-
-                    <button className="bg-secondary pl-8 pr-8 pt-2 pb-2 rounded-full text-white font-semibold -mt-2"> ADD </button>
-
-                </div>
-
-                <div className="bg-white w-2/3 h-1/4 mt-7 rounded-2xl z-30">
-                    <div className="flex flex-col justify-center items-center pt-5">
-                        <label htmlFor="weight" className="font-medium text-sm pr-44 mb-1">Weight:</label>
-                        <p 
-                        type="number" 
-                        value={weight} 
-                        className="w-4/5 bg-quinary rounded-md pl-2"> - </p>
-                    </div>
-
-                    <div className="flex flex-col justify-center items-center pt-4">
-                        <label htmlFor="date" className="font-medium text-sm pr-48 mb-1">Date:</label>
-                        <p 
-                        type="date" 
-                        value={date.toISOString().substring(0, 10)} 
-                        className="w-4/5 bg-quinary rounded-md pl-2"> - </p>
+                <div className="bg-white w-2/3 rounded-full z-20">
+                    <div className="flex text-s gap-1 font-medium p-1">
+                        <p className="w-48 bg-tertiary rounded-full text-white p-1"> Add </p>
+                        <p className="w-48 rounded-full p-1" onClick={handleHistory}> History </p>
                     </div>
                 </div>
 
-                <div className="bg-white w-2/3 h-1/4 mt-7 rounded-2xl z-30">
-                    <div className="flex flex-col justify-center items-center pt-5">
-                        <label htmlFor="weight" className="font-medium text-sm pr-44 mb-1">Weight:</label>
-                        <p 
-                        type="number" 
-                        value={weight} 
-                        className="w-4/5 bg-quinary rounded-md pl-2"> - </p>
-                    </div>
+                <br></br>
 
-                    <div className="flex flex-col justify-center items-center pt-4">
-                        <label htmlFor="date" className="font-medium text-sm pr-48 mb-1">Date:</label>
-                        <p 
-                        type="date" 
-                        value={date.toISOString().substring(0, 10)} 
-                        className="w-4/5 bg-quinary rounded-md pl-2"> - </p>
-                    </div>
-                </div>
+                <p className="text-base text-septenary font-semibold italic"> Add a new powder collection </p>
+
+                <br></br>
+
+                <PowderForm 
+                    weight={weight}
+                    handleWeightChange={handleWeightChange}
+                    driedDate={driedDate}
+                    handleDriedDateChange={handleDriedDateChange}
+                    flouredDate={flouredDate}
+                    handleFlouredDateChange={handleFlouredDateChange}
+                    handleSubmit={handleSubmit}
+                    formSubmitted={formSubmitted}
+                />
 
             </div>
             
