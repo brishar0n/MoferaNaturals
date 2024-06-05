41019bad3abf1503de92a5f3b5ccfa2412d90888
Jeff <jeffguo0927@gmail.com>
2024-06-05 07:41:00 +0700

    changed box width

--- frontend/src/components/centra/PackageBox.jsx
+++ frontend/src/components/centra/PackageBox.jsx
@@ -5,7 +5,7 @@ function PackageBox({ weight, expDate, status, id, shippingDate}) {
     const currentStatusStyle = statusStyles[status.toUpperCase()] || {};
 
     return (
-        <div className='bg-white mb-5 w-3/4 mx-auto py-5 px-7 rounded-2xl text-left relative flex flex-col'>
+        <div className='bg-white mb-5 mx-auto py-5 px-7 rounded-2xl text-left relative flex flex-col'>
             <div className="flex items-end justify-end w-full">
                 <p className="font-medium text-slate-500 text-xs">PKG#{id}</p>
             </div>
