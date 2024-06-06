5592bc1d7643979e7647770154dc4f4d7224da3e
Jeff <jeffguo0927@gmail.com>
2024-06-06 19:57:20 +0700

    added profile page

---
+++ frontend/src/pages/centra/Profile.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react' 
+import bgupper from '../../assets/profile/element1.svg'
+import bglower from '../../assets/profile/element2.svg'
+import NavigationBar from "../../components/centra/CentraNavbar.jsx";
+import { motion } from "framer-motion";
+import ProfileContent from '../../components/centra/ProfileContent';
+
+function Profile() {
+    const [isMobile, setIsMobile] = React.useState(false);
+    const [role, setRole] = useState("centra");
+    const [name, setName] = useState("Oowwwlaf");
+
+    useEffect(() => {
+      function handleResize() {
+        setIsMobile(window.innerWidth < 600);
+      }
+  
+      handleResize();
+      window.addEventListener("resize", handleResize);
+  
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+  
+    return (
+      <div className='bg-white w-screen h-screen overflow-auto'>
+        {isMobile && (
+          <>
+            <div className='h-screen absolute inset-0 flex'>
+              <img src={bgupper} className="w-screen fixed -top-10"/>
+              <img src={bglower} className="w-screen fixed bottom-0"/>
+              <img src="src/assets/AddPage/mascotAddSide.svg" className="absolute right-0 bottom-10 z-50"></img>
+            </div>
+            
+            <motion.div
+              key="profile"
+              initial={{ x: 300, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: -300, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+                <div className='relative z-40'>
+                    <ProfileContent role={role} name={name}/>
+                </div>
+              
+            </motion.div>
+            
+            <NavigationBar/> 
+          </>
+        )}
+      </div>
+    )
+}
+
+export default Profile;
