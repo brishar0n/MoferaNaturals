5ad2101fe3e6a1bef3883b8a7dcdc79beb7f8dd3
Jeff <jeffguo0927@gmail.com>
2024-06-16 00:12:54 +0700

    managed image imports

--- frontend/src/components/centra/CentraNavbar.jsx
+++ frontend/src/components/centra/CentraNavbar.jsx
@@ -3,6 +3,13 @@ import { Link } from 'react-router-dom';
 import Tooltip from "./Tooltip";
 import { motion } from 'framer-motion';
 
+// Import images
+import homeIcon from "../../assets/notifications/home.svg";
+import expeditionIcon from "../../assets/notifications/expedition.svg";
+import plusIcon from "../../assets/notifications/plus.svg";
+import bellIcon from "../../assets/notifications/bell.svg";
+import profileIcon from "../../assets/notifications/profile.svg";
+
 function NavigationBar(props) {
   const [overlay, setOverlay] = useState(false);
 
@@ -16,38 +23,37 @@ function NavigationBar(props) {
         <div className='flex gap-11 items-center'>
           <Link to="/centradashboard">
             <button>
-              <img src="src/assets/notifications/home.svg" className='w-7' alt='Home'></img>
+              <img src={homeIcon} className='w-7' alt='Home'></img>
             </button>
           </Link>
 
           <Link to="/trackshipping">
             <button>
-              <img src="src/assets/notifications/expedition.svg" className='w-7' alt='Shipping Info'></img>
+              <img src={expeditionIcon} className='w-7' alt='Shipping Info'></img>
             </button>
           </Link>
         </div>
 
         <div className='flex justify-center items-center'>
-          
           <Tooltip
             content="Add Wet Leaves &#010; Add Dry Leaves  &#010; Add Powder  &#010; Add Shipping Info  &#010;Add Package"
             direction="top"
             onToggle={handleOverlay}
           >
-            <img src="src/assets/notifications/plus.svg" className='w-20 cursor-pointer' alt='Plus'></img>
+            <img src={plusIcon} className='w-20 cursor-pointer' alt='Plus'></img>
           </Tooltip>
         </div>
 
         <div className='flex gap-11 items-center'>
           <Link to="/notify">
             <button>
-              <img src="src/assets/notifications/bell.svg" className='w-7' alt='Notifications'></img>
+              <img src={bellIcon} className='w-7' alt='Notifications'></img>
             </button>
           </Link>
 
           <Link to="/profile">
             <button>
-              <img src="src/assets/notifications/profile.svg" className='w-6' alt='Profile'></img>
+              <img src={profileIcon} className='w-6' alt='Profile'></img>
             </button>
           </Link>
         </div>
@@ -62,7 +68,6 @@ function NavigationBar(props) {
           transition={{ duration: 0.3 }}
         />
       )}
-
 
       {props.children}
     </>
