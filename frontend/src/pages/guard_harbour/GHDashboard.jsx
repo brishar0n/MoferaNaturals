d4329da8c131ca956e17371d8316d142b36af441
Jeff <jeffguo0927@gmail.com>
2024-06-17 04:56:51 +0700

    fixed gh dashboard

--- frontend/src/pages/guard_harbour/GHDashboard.jsx
+++ frontend/src/pages/guard_harbour/GHDashboard.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import GuardHarbourDashboardContent from "../../components/guard_harbour/GuardHarbourDashboardContent";
 import NavbarGH from "../../components/guard_harbour/NavbarGH";
 import { motion } from "framer-motion";
+import { getCurrentUser } from '../../../api/profileAPI';
 
 function GHDashboard() {
     const [isMobile, setIsMobile] = useState(false);
+    const [username, setUsername]= useState("");
 
     useEffect(() => {
         const handleResize = () => {
@@ -23,6 +25,21 @@ function GHDashboard() {
         };
     }, []);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const user = await getCurrentUser();
+                console.log(user);
+                
+                setUsername(user.username);
+            } catch (err) {
+                console.error("Error: ", err);
+            }
+        };
+    
+        fetchData();
+      }, []);
+
     return (
         <div className="bg-quaternary w-screen h-screen overflow-y-scroll flex-1">
             {isMobile && (
@@ -31,14 +48,14 @@ function GHDashboard() {
                     <img className="absolute" src={"src/assets/dashboard/vector-412.svg"}/>
 
                     <div className="relative mt-12 ml-7 font-medium text-left text-white">
-                        <p className="m-0">Hello,</p>
-                        <p className="m-0 mb-1">Si Gembrot</p>
+                        <p className="m-0 text-lg">Hello,</p>
+                        <p className="m-0 mb-1 text-lg">{username}</p>
                         <p className="m-0 text-2xl"><b>Guard Harbour</b></p>
                     </div>
 
                     <img className="absolute top-10 right-7 rounded-full w-11 h-11 object-cover" alt="" src={"src/assets/dashboard/ellipse-8@2x.png"}/>
                     <img className="absolute top-1/4 w-full h-[703px]" alt="" src={"src/assets/dashboard/rectangle-3.svg"}/>
-                    <img className="fixed bottom-0 w-full" alt="" src={"src/assets/dashboard/bottom-element.svg"}/>
+                   
                     <img className="absolute top-16 right-16 w-[91px] h-[179px] object-cover" alt="" src={"src/assets/dashboard/gembrot-4@2x.png"} />
 
                     <motion.div
@@ -52,6 +69,7 @@ function GHDashboard() {
                         
                     </motion.div>
 
+                    <img className="relative w-full top-20" alt="" src={"src/assets/dashboard/bottom-element.svg"}/>
                     <NavbarGH />
                 </div>
             )}
