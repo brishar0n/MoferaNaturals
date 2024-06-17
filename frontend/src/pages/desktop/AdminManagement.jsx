ed90ca977a0a54f3c54960ad529b4995e48d6489
I Kadek Gita Pradnya Widagda <skyb3105@gmail.com>
2024-06-18 00:42:36 +0700

    updateeeee

--- frontend/src/pages/desktop/AdminManagement.jsx
+++ frontend/src/pages/desktop/AdminManagement.jsx
@@ -1,236 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import "../../style/AdminDesktop.css"
-// import AdminTable from "../../components/admin/AdminTable";
-// import AdminTableUser from "../../components/admin/AdminTableUser";
-// import AddUserButton from "../../components/admin/AddUserButton";
-// import { columns, columnsCentra, columnsCheckpoint, columnsCollection, columnsDry, columnsFlour, columnsPackage, columnsShipping, columnsWet} from "../../components/admin/UserDataSample";
-// import AdminSidebar from "../../components/admin/AdminSidebar";
-// import SearchButtonData from "../../components/admin/SearchButtonData";
-// import MasterDataFolder from "../../components/admin/MasterDataFolder";
-// import PageTitleAll from "../../components/admin/PageTitleAll";
-// import UserProfile from "../../components/admin/UserProfile";
-// import DashboardContent from "../../components/admin/DashboardContent";
-// import {getUsers, getCentra, getCheckpoints, getDryLeaves, getFlour, getPackages, getShippingInfo, getWetLeaves, updateUser, deleteUser, deleteCentra, deleteCheckpoint, deleteDryLeaves, deleteFlour, deletePackage, deleteShippingInfo, deleteWetLeaves} from "../../../api/adminAPI";
-
-// const columnsMap = {
-//     AdminTable: columns,
-//     CentraData: columnsCentra,
-//     CheckpointData: columnsCheckpoint,
-//     CollectionData: columnsCollection,
-//     DryLeavesData: columnsDry,
-//     FlourData: columnsFlour,
-//     PackageData: columnsPackage,
-//     ShippingInfoData: columnsShipping,
-//     WetLeavesData: columnsWet,
-// };
-
-// function AdminPage() {
-//     const [rows, setRows] = useState([]);
-//     const [columnData, setColumnData] = useState(columnsMap.AdminTable);
-    
-//     const addUser = (newUser) => {
-//         console.log("Adding new user:", newUser);
-//         setRows((prevRows) => [
-//             ...prevRows,
-//             {
-//                 key: String(prevRows.length + 1),
-//                 id: newUser.id,
-//                 name: newUser.name,
-//                 email: newUser.email,
-//                 role: newUser.role,
-//             }
-//         ]);
-//     };
-
-//     const handleEditUser = async (updatedUser) => {
-//         try {
-//             delete updatedUser.hashed_password
-//             delete updatedUser.is_active
-//             await updateUser(updatedUser);
-//             console.log("Updated user:", updatedUser);
-//             setRows((prevUsers) =>
-//                 prevUsers.map((row) =>
-//                     row.id === updatedUser.id ? updatedUser : row
-//                 )
-//             );
-//         } catch (error) {
-//             console.error("Failed to update user:", error);
-//         }
-//     };
-
-//     const [isMinimized, setIsMinimized] = useState(false);
-
-//     const toggleMenu = () => {
-//         setIsMinimized(!isMinimized);
-//     };
-
-//     const [pageData, setPageData] = useState({
-//         title: 'Manage Users',
-//         description: 'Arrange username and data collections of ID'
-//     });
-    
-//     const [currentComponent, setCurrentComponent] = useState('AdminTable');
-
-//     const handlePageDataChange = (title, description, componentKey, rows, columnData) => {
-//         setPageData({ title, description });
-//         setCurrentComponent(componentKey);
-//         setRows(rows)
-//         setColumnData(columnData)
-//     };
-
-//     const deleteRow = async (id) => {
-//         try {
-//             switch (currentComponent) {
-//                 case 'AdminTable':
-//                     await deleteUser(id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     break;
-//                 case 'CentraData':
-//                     try{
-//                     await deleteCentra(id);
-//                     console.log("Deleted Centra with id:", id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     }catch(error){
-//                         console.error("Failed to delete user:", error);
-//                     }
-//                     break;
-//                 case 'CheckpointData':
-//                     await deleteCheckpoint(id);
-//                     console.log("Deleted Checkpoint with id:", id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     break;
-//                 case 'WetLeavesData':
-//                     await deleteWetLeaves(id);
-//                     console.log("Deleted Wet Leaves with id:", id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     break;
-//                 case 'DryLeavesData':
-//                     await deleteDryLeaves(id);
-//                     console.log("Deleted Dry Leaves with id:", id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     break;
-//                 case 'FlourData':
-//                     await deleteFlour(id);
-//                     console.log("Deleted Flour with id:", id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     break;
-//                 case 'ShippingInfoData':
-//                     await deleteShippingInfo(id);
-//                     console.log("Deleted Shipping Info with id:", id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     break;
-//                 case 'PackageData':
-//                     await deletePackage(id);
-//                     console.log("Deleted Package with id:", id);
-//                     setRows(prevRows => prevRows.filter(row => row.id !== id));
-//                     break;
-//                 default:
-//                     break;
-//             }
-
-//         } catch (error) {
-//             console.error("Failed to delete user:", error);
-//         }
-//     };
-
-//     const handleSearch = (searchTerm) => {
-//         // Filter rows based on search term
-//         const filteredData = rows.filter((row) =>
-//             Object.values(row).some((value) =>
-//                 String(value).toLowerCase().includes(searchTerm.toLowerCase())
-//             )
-//         );
-//         setFilteredRows(filteredData); // Update filtered rows state
-//     };
-
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             let data = [];
-//             switch (currentComponent) {
-//                 case 'AdminTable':
-//                     data = await getUsers();
-//                     break;
-//                 case 'CentraData':
-//                     data = await getCentra();
-//                     break;
-//                 case 'CheckpointData':
-//                     data = await getCheckpoints();
-//                     break;
-//                 case 'WetLeavesData':
-//                     data = await getWetLeaves();
-//                     break;
-//                 case 'DryLeavesData':
-//                     data = await getDryLeaves();
-//                     break;
-//                 case 'FlourData':
-//                     data = await getFlour();
-//                     break;
-//                 case 'ShippingInfoData':
-//                     data = await getShippingInfo();
-//                     break;
-//                 case 'PackageData':
-//                     data = await getPackages();
-//                     break;
-//                 default:
-//                     data = await getUsers();
-//                     break;
-//             }
-//             setRows(data.data);
-//             setColumnData(columnsMap[currentComponent]); // Update columns if needed
-//         };
-//         fetchData();
-//     }, [currentComponent]);
-    
-//     const renderComponent = () => {
-//         switch (currentComponent) {
-//             case 'AdminTable':
-//                 return <AdminTableUser columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             case 'MasterDataFolder':
-//                 return <MasterDataFolder />;
-//             case 'Dashboard':
-//                 return <DashboardContent/>
-//             case 'CentraData':
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             case 'WetLeavesData':
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             case 'DryLeavesData':
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             case 'FlourData':
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             case 'ShippingInfoData':
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             case 'CheckpointData':
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             case 'PackageData':
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//             default:
-//                 return <AdminTable columns={columnData} rows={rows} deleteRow={deleteRow} editRow={handleEditUser} />;
-//         }
-//     };
-//     return (
-//         <div className="flex justify-start items-center bg-primary h-screen w-screen">
-//             <AdminSidebar isMinimized={isMinimized} toggleMenu={toggleMenu} onPageDataChange={handlePageDataChange}/>
-//             <div className={`bg-white h-97vh rounded-3xl transition-all duration-300 ${isMinimized ? 'w-19/20' : 'w-41/50'}`}>
-//                 <PageTitleAll title={pageData.title} description={pageData.description}/>
-//                 <UserProfile/>
-//                 <SearchButtonData onSearch={handleSearch}/>
-//                 <div className={`flex justify-start pt-10 items-center gap-8 ${pageData.title !== 'Data Master' && pageData.title !== 'Dashboard' ? 'pl-0' : 'pl-12'}`}>
-                    
-//                     {/* <MasterDataFolder/> */}
-
-//                     {/* <AdminTable rows={rows} setRows={setRows} deleteRow={deleteRow} editRow={handleEditUser}/>  */}
-//                     {renderComponent()}
-//                 </div>
-
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default AdminPage;
-
-
 import React, { useEffect, useState } from "react";
 import "../../style/AdminDesktop.css";
 import AdminTable from "../../components/admin/AdminTable";
@@ -297,36 +64,32 @@ function AdminPage() {
   });
 
   const addUser = (newUser) => {
-        console.log("Adding new user:", newUser);
-        setRows((prevRows) => [
-         ...prevRows,
-        {
+    console.log("Adding new user:", newUser);
+    setRows((prevRows) => [
+      ...prevRows,
+      {
         key: String(prevRows.length + 1),
         id: newUser.id,
         name: newUser.name,
         email: newUser.email,
         role: newUser.role,
-        }
+      },
     ]);
   };
 
-  
   const handleEditUser = async (updatedUser) => {
     try {
-        delete updatedUser.hashed_password
-        delete updatedUser.is_active
-        await updateUser(updatedUser);
-        console.log("Updated user:", updatedUser);
-        setRows((prevUsers) =>
-            prevUsers.map((row) =>
-                row.id === updatedUser.id ? updatedUser : row
-            )
-        );
-        } catch (error) {
-        console.error("Failed to update user:", error);
+      delete updatedUser.hashed_password;
+      delete updatedUser.is_active;
+      await updateUser(updatedUser);
+      console.log("Updated user:", updatedUser);
+      setRows((prevUsers) =>
+        prevUsers.map((row) => (row.id === updatedUser.id ? updatedUser : row))
+      );
+    } catch (error) {
+      console.error("Failed to update user:", error);
     }
   };
-    
 
   const [currentComponent, setCurrentComponent] = useState("AdminTable");
 
@@ -344,6 +107,7 @@ function AdminPage() {
     setPageData({ title, description });
     setCurrentComponent(componentKey);
     setRows(rows);
+    setFilteredRows([]);
     setColumnData(columnData);
   };
 
@@ -404,7 +168,7 @@ function AdminPage() {
         String(value).toLowerCase().includes(searchTerm.toLowerCase())
       )
     );
-    setFilteredRows(filteredData); // Update filtered rows state
+    setFilteredRows(filteredData);
   };
 
   useEffect(() => {
@@ -440,6 +204,7 @@ function AdminPage() {
           break;
       }
       setRows(data.data);
+      setFilteredRows([]); // Reset filteredRows when new data is fetched
       setColumnData(columnsMap[currentComponent]); // Update columns if needed
     };
     fetchData();
@@ -459,8 +224,7 @@ function AdminPage() {
       case "MasterDataFolder":
         return <MasterDataFolder />;
       case "Dashboard":
-        // return <DashboardContent />;
-        return <DashboardDataFolder/>
+        return <DashboardDataFolder />;
       case "CentraData":
         return (
           <AdminTable
@@ -487,7 +251,7 @@ function AdminPage() {
             deleteRow={deleteRow}
             editRow={handleEditUser}
           />
-        );
+        )
       case "FlourData":
         return (
           <AdminTable
@@ -544,12 +308,11 @@ function AdminPage() {
         onPageDataChange={handlePageDataChange}
       />
       <div
-        className={`bg-white h-97vh rounded-3xl transition-all duration-300 ${ isMinimized ? "w-19/20" : "w-41/50"}`}
+        className={`bg-white h-97vh rounded-3xl transition-all duration-300 ${
+          isMinimized ? "w-19/20" : "w-41/50"
+        }`}
       >
-        <PageTitleAll
-          title={pageData.title}
-          description={pageData.description}
-        />
+        <PageTitleAll title={pageData.title} description={pageData.description} />
         <UserProfile />
         <SearchButtonData onSearch={handleSearch} />
         <div
@@ -567,4 +330,3 @@ function AdminPage() {
 }
 
 export default AdminPage;
-
