c4e80137d90744c0002c0661cae6eddea5caf891
Jeff <jeffguo0927@gmail.com>
2024-06-17 16:27:43 +0700

    added xyz dashboard

--- frontend/src/components/xyz/xyz_mobile/NavbarXYZ.jsx
+++ frontend/src/components/xyz/xyz_mobile/NavbarXYZ.jsx
@@ -1,16 +1,26 @@
 import rescale from '../../../assets/xyz/rescale.svg';
 import reception from '../../../assets/xyz/reception.svg';
+import profile from '../../../assets/notifications/profile.svg';
+import home from '../../../assets/notifications/home.svg';
 import { Link } from "react-router-dom";
 
 function NavbarXYZ() {
     return (
-        <div className="bg-secondary flex flex-row justify-evenly py-6 w-full bottom-0 rounded-t-3xl fixed">
+        <div className="bg-secondary flex flex-row justify-evenly py-6 w-full bottom-0 rounded-t-3xl fixed z-50 h-24 items-center">
+            <Link to="/xyzdashboard">
+                <button><img src={home} alt="rescale" className='w-9'/></button>
+            </Link>
+
             <Link to="/findrescale">
-                <button><img src={rescale} alt="rescale" className='w-12'/></button>
+                <button><img src={rescale} alt="rescale" className='w-9'/></button>
             </Link>
         
             <Link to="/receptionpackage">
-                <button><img src={reception} alt="reception" className='w-12'/></button>
+                <button><img src={reception} alt="reception" className='w-9'/></button>
+            </Link>
+
+            <Link to="/profile">
+                <button><img src={profile} alt="profile" className='w-7'/></button>
             </Link>
         </div>
     )
