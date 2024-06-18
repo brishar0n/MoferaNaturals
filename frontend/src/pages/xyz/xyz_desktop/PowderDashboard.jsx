2a1b825da20f3f1321a6cfa1c71ca26757da9e7f
Ëzo-Tanuki <michailbryanlie@gmail.com>
2024-06-18 22:16:48 +0700

    lupa push sori heehhee

--- frontend/src/pages/xyz/xyz_desktop/PowderDashboard.jsx
+++ frontend/src/pages/xyz/xyz_desktop/PowderDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Sidebar from "../../../components/xyz/Sidebar";
 import BarChart from "./BarChart";
 import AreaChart from "./AreaChart";
-import Table from "./Table";
+import Table from "./TablePowder";
 import RecentActivities from "./RecentActivities";
 import SearchForm from "./Search";
 import { IoNotifications } from "react-icons/io5";
