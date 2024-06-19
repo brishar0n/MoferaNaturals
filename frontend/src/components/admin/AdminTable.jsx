b110adce37afa3b52e030731f3d785fafa556fd6
I Kadek Gita Pradnya Widagda <skyb3105@gmail.com>
2024-06-19 22:38:41 +0700

    extra poin pls +100pts

--- frontend/src/components/admin/AdminTable.jsx
+++ frontend/src/components/admin/AdminTable.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Button, Pagination} from "@nextui-org/react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Button,
+  Pagination,
+} from "@nextui-org/react";
 import "../../style/AdminDesktop.css";
-import { initialRows, columns} from "./UserDataSample";
+import { initialRows, columns } from "./UserDataSample";
 import EditUserButton from "./EditUserButton";
 
 const centraOptions = Array.from({ length: 32 }, (_, i) => i + 1);
 
-function AdminTable({ rows , columns , deleteRow, editRow, pageName}) {
-  const [currentPage, SetCurrentPage] = useState(1)
+function AdminTable({ rows, columns, deleteRow, editRow, pageName }) {
+  const [currentPage, SetCurrentPage] = useState(1);
   const [sortBy, setSortBy] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
   const [selectedCentra, setSelectedCentra] = useState(0);
@@ -15,7 +25,7 @@ function AdminTable({ rows , columns , deleteRow, editRow, pageName}) {
   const itemsPerPage = 5;
 
   const handleDelete = (id) => {
-    deleteRow(id)
+    deleteRow(id);
   };
 
   const handleEdit = (updatedUser) => {
@@ -27,115 +37,64 @@ function AdminTable({ rows , columns , deleteRow, editRow, pageName}) {
   };
 
   const handlePageChange = (page) => {
-    SetCurrentPage(page)
-  }
-  const handleFilterChange = (sortingBy) => {
-    setSortBy(sortingBy);
+    SetCurrentPage(page);
   };
-  
+
+  const handleSort = (column) => {
+    const newSortOrder = sortBy === column && sortOrder === "asc" ? "desc" : "asc";
+    setSortBy(column);
+    setSortOrder(newSortOrder);
+  };
+
   let filteredRows;
 
-  if (selectedCentra != 0) {
+  if (selectedCentra !== 0) {
     filteredRows = rows.filter((row) => String(row.centra_id) === selectedCentra);
-  } else if (sortBy == 'status') {
-    filteredRows = rows.filter((row) => row.status === sortBy);
   } else {
     filteredRows = rows;
   }
-  
+
   const sortedData = [...filteredRows].sort((a, b) => {
-    if (sortBy === "") return;
-    const aValue = sortBy === "created_datetime" ? new Date(b[sortBy]) : a[sortBy];
-    const bValue = sortBy === "created_datetime" ? new Date(a[sortBy]) : b[sortBy];
+    if (!sortBy) return 0;
+    const aValue = sortBy === "created_datetime" ? new Date(a[sortBy]) : a[sortBy];
+    const bValue = sortBy === "created_datetime" ? new Date(b[sortBy]) : b[sortBy];
     if (aValue < bValue) return sortOrder === "asc" ? -1 : 1;
     if (aValue > bValue) return sortOrder === "asc" ? 1 : -1;
     return 0;
   });
-  
+
   const totalPages = Math.ceil(sortedData.length / itemsPerPage);
   const paginatedRows = sortedData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
   return (
-    <div className="flex flex-col w-97/100 pl-14 items-center gap-8 border-collapse" >
+    <div className="flex flex-col w-97/100 pl-14 items-center gap-8 border-collapse">
       <div className="flex justify-end w-full">
-        {pageName != 'CentraData' && pageName != 'CheckpointData' ? 
-            <form className="w-40">
-              <select
-                className="flex bg-quinary border border-primary text-primary text-sm focus:ring-primary focus:border-primary block p-1 dark:bg-primary dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:primary dark:focus:border-primary rounded-full px-1 mr-4"
-                onChange={handleCentraChange}
-                value={selectedCentra}
-              >
-                <option value="">Select Centra</option>
-                {centraOptions.map((centra) => (
-                  <option key={centra} value={centra}>
-                    Centra {centra}
-                  </option>
-                ))}
-              </select>
-            </form>
-            :
-            null
-          }
-        {pageName !== 'CentraData' && pageName !== 'ShippingInfoData' && pageName != 'CheckpointData' ? 
-          <form onSubmit={(e) => e.preventDefault()}>
+        {pageName !== "CentraData" && pageName !== "CheckpointData" ? (
+          <form className="w-40">
             <select
               className="flex bg-quinary border border-primary text-primary text-sm focus:ring-primary focus:border-primary block p-1 dark:bg-primary dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:primary dark:focus:border-primary rounded-full px-1 mr-4"
-              value={sortBy}  
-              onChange={(e) => handleFilterChange(e.target.value)}
+              onChange={handleCentraChange}
+              value={selectedCentra}
             >
-              <option value="">Sort By</option>
-              {pageName === 'DryLeavesData' || pageName === 'WetLeavesData' || pageName == 'FlourData' ? 
-                <option value="weight">Weight</option>
-                : 
-                null
-              }
-              
-              {pageName === 'PackageData' ? 
-                <option value="0">Ready to Ship</option>
-                :
-                null
-              }
-
-              {pageName === 'PackageData' ? 
-                <option value="1">Shipping</option>
-                :
-                null
-              }
-
-              {pageName === 'PackageData' ? 
-                <option value="2">Confirmed Arrival</option>
-                :
-                null
-              }
-
-              {pageName === 'PackageData' ? 
-                <option value="3">Collected</option>
-                :
-                null
-              }
-
-              {pageName === 'PackageData' ? 
-                <option value="4">Expired</option>
-                :
-                null
-              }
-
-              {pageName === 'PackageData' ? 
-                <option value="created_datetime">Created Date</option>
-                :
-                null
-              }
+              <option value="">Select Centra</option>
+              {centraOptions.map((centra) => (
+                <option key={centra} value={centra}>
+                  Centra {centra}
+                </option>
+              ))}
             </select>
           </form>
-          :
-            null
-        }
+        ) : null}
       </div>
 
       <Table aria-label="Example table with dynamic content" className="text-center drop-shadow-md border-collapse">
         <TableHeader columns={columns}>
           {(column) => (
-            <TableColumn key={column.key} className="bg-quinary text-black text-center text-sm">
+            <TableColumn
+              key={column.key}
+              className="bg-quinary text-black text-center text-sm cursor-pointer"
+              onClick={() => handleSort(column.key)}
+            >
               {column.label}
             </TableColumn>
           )}
@@ -147,12 +106,11 @@ function AdminTable({ rows , columns , deleteRow, editRow, pageName}) {
               {(columnKeys) => (
                 <TableCell className="text-base justify-center">
                   {columnKeys.key === "created_datetime"
-                      ? formatDateTime(item[columnKeys.key])
-                      : columnKeys.key === "status"
-                      ? getStatusLabel(item[columnKeys.key])
-                      : item[columnKeys.key]
-                  }
-                  {columnKeys === "actions" ? (
+                    ? formatDateTime(item[columnKeys.key])
+                    : columnKeys.key === "status"
+                    ? getStatusLabel(item[columnKeys.key])
+                    : item[columnKeys.key]}
+                  {columnKeys.key === "actions" ? (
                     <div className="flex gap-2 justify-center">
                       <Button
                         size="md"
@@ -174,18 +132,18 @@ function AdminTable({ rows , columns , deleteRow, editRow, pageName}) {
           )}
         </TableBody>
       </Table>
-    
-      <Pagination 
-       isCompact
-       showControls
-       total={totalPages}
-       onChange={handlePageChange}
-       color="quinary"
-       className="drop-shadow-lg"
+
+      <Pagination
+        isCompact
+        showControls
+        total={totalPages}
+        onChange={handlePageChange}
+        color="quinary"
+        className="drop-shadow-lg"
       />
-      
     </div>
   );
 }
 
 export default AdminTable;
+
